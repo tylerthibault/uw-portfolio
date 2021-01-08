0d315984e93ba13a51b5328c491013e9be83cc8e
Tyler <thibault.tyler@gmail.com>
2021-01-07 23:50:21 -0800

    updated splashscreen

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ $(document).ready(function () {
 
     typeItOut()
 
+    $('.main-content').on("click", () => {
+        console.log('click');
+        let sideMenu = $('.side-menu').data('status')
+        console.log(sideMenu);
+        if ($('.side-menu').data('status') === 'open') {
+            // closeSideMenu()
+        }
+    })
 
     // title being sticky on small screens
     var screenSize = window.screen.width;
@@ -19,48 +27,47 @@ $(document).ready(function () {
     setColors(color);
 
     $(".colorMode").on("click", (e) => {
-        console.log("the color is: " + color);
         color = switchColor(color);
         setColors(color);
     });
 
     // small screen side menu
     $(".menuBtn").on("click", (e) => {
-        // check navMenu display status
-        var sideMenu = $(".side-menu");
-        console.log($(sideMenu).data("status"));
-        if (sideMenu.data("status") === "close") {
-            $(sideMenu).addClass("side-menu-open");
-            $(sideMenu).removeClass("side-menu-close");
-            $(sideMenu).css("display", "block");
-            $(sideMenu).data("status", "open");
-        } else {
-            $(sideMenu).addClass("side-menu-close");
-            $(sideMenu).removeClass("side-menu-open");
-            $(sideMenu).data("status", "close");
-            setTimeout(() => {
-                $(sideMenu).css("display", "none");
-            }, 2000);
-        }
+        closeSideMenu()
     });
 });
 
 // functions
 
+function closeSideMenu(){ 
+    // check navMenu display status
+    var sideMenu = $(".side-menu");
+    if (sideMenu.data("status") === "close") {
+        $(sideMenu).addClass("side-menu-open");
+        $(sideMenu).removeClass("side-menu-close");
+        $(sideMenu).css("display", "block");
+        $(sideMenu).data("status", "open");
+    } else {
+        $(sideMenu).addClass("side-menu-close");
+        $(sideMenu).removeClass("side-menu-open");
+        $(sideMenu).data("status", "close");
+        setTimeout(() => {
+            $(sideMenu).css("display", "none");
+        }, 2000);
+    }
+}
+
+
 function getColor() {
     try {
         var colorMode = localStorage.getItem("colorMode");
-        console.log("try");
     } catch {
         var colorMode = "dark";
-        console.log("catch");
     }
-    console.log("color mode is: " + colorMode);
     return colorMode;
 }
 
 function switchColor(color) {
-    console.log("switch color: " + color);
     if (color === "dark") {
         color = "light";
         localStorage.setItem("colorMode", "light");
@@ -68,15 +75,12 @@ function switchColor(color) {
         color = "dark";
         localStorage.setItem("colorMode", "dark");
     }
-    console.log("returning the color " + color);
     return color;
 }
 
 function setColors(color) {
-    console.log("set color: " + color);
     var r = document.querySelector(":root");
     if (color === "dark") {
-        console.log("change to light mode");
         $(".colorMode").text("Dark Mode");
         r.style.setProperty("--color1", "#d8f3dc");
         r.style.setProperty("--color2", "#b7e4c7");
@@ -84,7 +88,6 @@ function setColors(color) {
         r.style.setProperty("--color4", "#74c69d");
         r.style.setProperty("--color5", "#52b788");
     } else {
-        console.log("change to dark mode");
         $(".colorMode").text("Light Mode");
         r.style.setProperty("--color1", "#264653");
         r.style.setProperty("--color2", "#2a9d8f");
@@ -119,12 +122,11 @@ function typeItOut(){
             } else {
                 listCount++
             }
-            console.log('end of word');
             sleep(1000)
             txt_container.text('')
             i=0
         }
-    }, 100)
+    }, 150)
 }
 
 function sleep(milliseconds) {
